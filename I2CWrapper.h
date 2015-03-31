868855dcca31adb76ff94782afa7bd0d2e3da3c6
Corentin Smith <corentin.smith@gmail.com>
2015-03-31 15:56:27 +0200

    Create I2CWrapper.h

---
+++ I2CWrapper.h
@@ -0,0 +1,36 @@
+#ifndef I2CWrapper_h
+#define I2CWrapper_h
+
+#include <Wire.h>
+
+void i2cWrite(uint8_t address, uint8_t registerAddress, uint8_t data) {
+    Wire.beginTransmission(address);
+    Wire.write(registerAddress);
+    Wire.write(data);
+    Wire.endTransmission();
+}
+
+uint8_t* i2cRead(uint8_t address, uint8_t registerAddress, uint8_t count, uint8_t * dest)
+{
+    Wire.beginTransmission(address);   // Initialize the Tx buffer
+    Wire.write(registerAddress);       // Put slave register address in Tx buffer
+    Wire.endTransmission(false);       // Send the Tx buffer, but send a restart to keep connection alive
+    uint8_t i = 0;
+    Wire.requestFrom(address, count);  // Read bytes from slave register address
+    while (Wire.available()) {
+        dest[i++] = Wire.read();       // Put read results in the Rx buffer
+    }
+}
+
+uint8_t i2cReadOne(uint8_t address, uint8_t registerAddress)
+{
+    uint8_t data; // `data` will store the register data
+    Wire.beginTransmission(address);         // Initialize the Tx buffer
+    Wire.write(registerAddress);             // Put slave register address in Tx buffer
+    Wire.endTransmission(false);             // Send the Tx buffer, but send a restart to keep connection alive
+    Wire.requestFrom(address, (uint8_t) 1);  // Read one byte from slave register address
+    data = Wire.read();                      // Fill Rx buffer with result
+    return data;                             // Return data read from slave register
+}
+
+#endif
